2ec3a8ee3927bf2f3371bf304db1dd6097853dc9
noinvis <lazizbek.sn@gmail.com>
2025-09-06 16:18:56 +0500

    fixed

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -2,8 +2,12 @@ import { memo } from "react";
 import UserView from "../user-view/UserView";
 
 const Main = async () => {
-  const response = await fetch("https://68ad7902a0b85b2f2cf37bcd.mockapi.io/user");
+  const response = await fetch(
+    "https://68ad7902a0b85b2f2cf37bcd.mockapi.io/user",
+    { cache: "no-store" }
+  );
   const data = await response.json();
+
   return (
     <div className="container">
       <p className="text-center text-[30px] max-[555px]:text-[1rem] font-medium">
